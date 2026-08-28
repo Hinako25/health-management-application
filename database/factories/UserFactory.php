e236ad41d6041c7927f3f7283217932bef915cac
fight-hinako <pinapinahinako21@gmail.com>
2026-08-28 19:05:11 +0900

    name属性がないと動かないためnameを追加。

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'email' => fake()->unique()->safeEmail(),
+            'email' => $email = fake()->unique()->safeEmail(),
+            'name' => $email,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
