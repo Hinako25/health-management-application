36a2130a4518a07903c35aaf689ec740a6153722
fight-hinako <pinapinahinako21@gmail.com>
2026-09-03 15:31:34 +0900

    ログイン画面とアカウント作成画面の右側を、サイト説明内容に変更。

--- resources/views/createaccount.blade.php
+++ resources/views/createaccount.blade.php
@@ -66,22 +66,38 @@
         </div>
         <div class="relative w-full min-h-screen md:block md:w-[50%] overflow-hidden">
             <div class="absolute inset-0 w-full h-screen flex flex-col justify-center items-center">
-                <div class="flex flex-col gap-4 w-2/3">
-                  <p class="text-center text-xl font-bold text-yellow-500 opacity-75 block">仕事や勉強の合間に少しストレッチを行うことで<br>肩や首の痛みを改善しませんか？</p>
+             <div class="flex flex-col gap-4 w-2/3 ml-2">
+                   <p class="text-left text-2xl font-bold text-green-600 block">あなたの健康習慣をサポートする<br>シンプルなタスク管理アプリ</p>
                    <br>
-                   <p class="text-left text-sm mx-5 block">
-                    このサイトは設定した期間内で仕事や勉強時間を計り、休憩時間にストレッチを行うサイトです。
-                    <br>
-                    実際に指定したストレッチが3分程度あり、その後自分の指定した時間勉強や仕事を好きなタイミングで行うことができます。
-                    </p>
-                    <br>
-                    <div class="w-full h-full aspect-3/2 rounded-sm flex justify-center items-center relative z-10">
-                  <img src="{{ asset('img/trialimg.png') }}" alt="ストレッチ画像" class="w-full h-full object-contain object-center">
+                   <div class="flex items-center gap-2">
+                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-12 border-2 border-green-600 rounded-full ml-5 text-green-600">
+                       <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                     </svg>
+                      <div class="flex flex-col">
+                        <p class="text-left text-xl text-green-600 mx-5 block">作業と休憩の時間を管理</p>
+                        <p class="text-left text-sm text-green-500  mx-5 block">集中とストレッチのバランスをとる</p>
+                     </div>
                     </div>
-                </div>
-
+                    <br>
+                    <div class="flex items-center gap-3">
+                     <img src="{{ asset('img/strechimg.png') }}" alt="ストレッチ" class="w-12 h-12 ml-5 opacity-75 rounded-full">
+                     <div class="flex flex-col">
+                        <p class="text-left text-xl text-green-600 mx-5 block">3分のストレッチで肩こり・首の痛みを直す</p>
+                        <p class="text-left text-sm text-green-500 mx-5 block">休憩時間に体をリフレッシュ<br>※画像はイメージです</p>
+                     </div>
+                    </div>
+                    <br>
+                    <div class="flex items-center gap-3">
+                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-12 border-2 border-green-600 rounded-full ml-5 text-green-600">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
+                     </svg>
+                     <div class="flex flex-col">    
+                        <p class="text-left text-xl text-green-600 mx-5 block">習慣の記録を可視化</p>
+                        <p class="text-left text-sm text-green-500  mx-5 block">続けるほどに変化がわかる</p>
+                     </div>
+                    </div>
+             </div>
             </div>
-
             <img    
                 src="{{ asset('img/bgdesign.png') }}"
                 alt=""
